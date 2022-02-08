b97e838754ec87560b2b60cf58b971f438961316
LucasAccurcio <lucas.accurcio@gmail.com>
2022-02-08 19:01:24 -0300

    Correcao funçao removeUser

--- src/controllers/removeUser.js
+++ src/controllers/removeUser.js
@@ -2,7 +2,7 @@ const { Users } = require('../models');
 
 module.exports = async (req, res) => {
   try {
-    const { id } = req.body;
+    const { id } = req.user;
     await Users.destroy({ where: { id } });
 
     res.status(204).end();
